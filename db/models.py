24f1dd8db04e71e0ce11e040f33a92d95d1de394
nastya <hociy2015@gmail.com>
2025-08-20 20:17:21 +0300

    Solution

--- db/models.py
+++ db/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
+
+
+class User(AbstractUser):
+    def __str__(self):
+        return self.username
 
 
 class Genre(models.Model):
@@ -17,7 +24,7 @@ class Actor(models.Model):
 
 
 class Movie(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, db_index=True)
     description = models.TextField()
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
@@ -50,3 +57,46 @@ class MovieSession(models.Model):
 
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="orders")
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self) -> str:
+        return f"<Order: {self.created_at.strftime('%Y-%m-%d %H:%M:%S')}>"
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(
+        to=MovieSession, on_delete=models.CASCADE, related_name="tickets"
+    )
+    order = models.ForeignKey(
+        to="Order", on_delete=models.CASCADE, related_name="tickets"
+    )
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["movie_session", "row", "seat"],
+                name="unique_movie_session_row_seat"
+            )
+        ]
+
+    def __str__(self):
+        return f"<Ticket: {self.movie_session} (row: {self.row}, seat: {self.seat})>"
+
+    def clean(self):
+        cinema_hall = self.movie_session.cinema_hall
+        if not (1 <= self.row <= cinema_hall.rows):
+            raise ValidationError("Row must be between 1 and Cinema Hall")
+        if not (1 <= self.seat <= cinema_hall.seats_in_row):
+            raise ValidationError("Seat must be between 1 and Cinema Hall")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
--- settings.py
+++ settings.py
@@ -6,6 +6,12 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # SECURITY WARNING: Modify this secret key if using in production!
 SECRET_KEY = "6few3nci_q_o@l1dlbk81%wcxe!*6r29yu629&d97!hiqat9fa"
 
+DEBUG = True
+
+
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 DATABASES = {
@@ -24,5 +30,10 @@ USE_I18N = True
 USE_TZ = False
 
 INSTALLED_APPS = [
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
     "db",
 ]
+
+
+AUTH_USER_MODEL = "db.User"
